1489f3508a19f7766ed09e2ddb5eb7b85f808058
Vilight Wu <vilight@Vilights-MacBook-Pro.local>
2015-02-15 13:22:27 -0800

    change to requirejs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,26 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
+var bower = '../../bower_components/';
+
+requirejs.config({
+    paths: {
+        jquery: bower + 'jquery/dist/jquery'
+    }
+});
+
+require(['jquery', 'sudoku'], function($, Sudoku){
+    $( document ).ready( function() {
+        var game = Sudoku.getInstance();
+        $( '#container').append( game.getGameBoard() );
+
+        $( '#solve').click( function() {
+            game.solve();
+        } );
+        $( '#validate').click( function() {
+            game.validate();
+        } );
+        $( '#reset').click( function() {
+            game.reset();
+        } );
+    } );
+});
+
