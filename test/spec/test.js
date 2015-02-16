d4640c0b6905fa19414a1d56a1aa145452f1b7eb
Vilight Wu <vilight@Vilights-MacBook-Pro.local>
2015-02-15 17:18:53 -0800

    add unit test mocha

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,41 @@
 /* global describe, it */
+require.config({
+    paths: {
+        sudoku: '../../app/scripts/sudoku',
+        jquery: '../../bower_components/jquery/dist/jquery'
+    }
+});
 
-(function () {
-  'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+define(function(require) {
+    var Sudoku = require('sudoku');
+    var game = Sudoku.getInstance();
 
+    'use strict';
+    describe('Sudoku Unit Tests', function () {
+      it('should be an object', function() {
+          expect(game).to.be.a('object');
       });
+
+      it('should have public method getGameBoard', function() {
+          game.should.have.property('getGameBoard');
+      });
+
+      it('should have public method reset', function() {
+          game.should.have.property('reset');
+      });
+
+      it('should have public method solve', function() {
+          game.should.have.property('solve');
+      });
+
+      it('should have public method validate', function() {
+          game.should.have.property('validate');
+      });
+
+
     });
-  });
-})();
+
+    mocha.run();
+ 
+});
